98c98f274653d302a50c49f0ad791d6c3171a0f2
Isabelle Pires Vimercati <isabellevimercati@ocyan-sa.com>
2026-03-23 20:42:43 -0300

    configurações iniciais do projeto

---
+++ src/main/java/com/tcc/talkie/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.tcc.talkie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcc.talkie.service.HelloWorldService;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    /* Injeção de dependencias */
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+
+    @GetMapping
+    public String helloWorld(){
+        return helloWorldService.helloWorld("Belle");
+    }
+}
---
+++ src/main/java/com/tcc/talkie/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.tcc.talkie.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloWorld(String name){
+        return "Hello World " + name;
+    }
+}
